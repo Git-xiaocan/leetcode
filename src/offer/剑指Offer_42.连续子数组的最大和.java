34d18720145333dfce6c4739a2d3308192c920b1
xiaocan <940659468@qq.com>
2021-08-31 09:49:03 +0800

    修改相关问题

--- src/offer/剑指Offer_42.连续子数组的最大和.java
+++ src/offer/剑指Offer_42.连续子数组的最大和.java
@@ -18,7 +18,7 @@
 class Solution {
     public static int maxSubArray(int[] nums) {
         int[] dp = new int[nums.length];
-        dp[0] = nums[0];
+   
         int max = Integer.MIN_VALUE;
         for (int i = 1; i < nums.length; i++) {
             dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
